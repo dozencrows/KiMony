722d4a79559c142c14a7c77386baa0750698134c
dozencrows <github@magwitch.uk.net>
2015-05-25 19:30:37 +0100

    WIP on remote data update

--- Tools/kimony.py
+++ Tools/kimony.py
@@ -3,6 +3,47 @@
 # Tool for updating KiMony data over USB serial
 #
 
+#
+# KiMony structures:
+#   IrCode: 
+#       bitfield    encoding:4 bits:5 code:23
+#       uint32_t    toggleMask
+#
+#   IrAction:
+#       int         code count
+#       IrCode[]    codes
+#
+#   Event:
+#       uint32_t    type:   0 (none), 1 (IrAction), 2 (Activity), 3 (NextPage), 4 (PrevPage), 5 (Home)
+#       union       IrAction* irAction, Activity* activity
+#
+#   ButtonMapping:
+#       uint32_t    buttonMask
+#       Event*      event
+#
+#   TouchButton:
+#       Event*      event
+#       char*       text
+#       uint16_t    x, y
+#       uint16_t    width, height
+#       uint16_t    colour
+#       uint8_t     flags           bit 0: press activates, bit 1: centre text
+#
+#   TouchButtonPage:
+#       int             count
+#       TouchButtons*   buttons
+#
+#   Activity:
+#       int                 buttonMappingCount
+#       ButtonMappng*       buttonMappings
+#       int                 touchButtonPageCount
+#       TouchButtonPage*    touchButtonPages
+
+# Key aspects:
+#   bitfield packing
+#   aligning
+#   using offsets instead of pointers
+
 import serial
 import struct
 import time
