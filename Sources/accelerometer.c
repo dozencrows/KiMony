098a7a3d1dcb66db1013eead1e521c2e2b0a95cd
dozencrows <github@magwitch.uk.net>
2015-06-26 11:31:37 +0100

    WIP: wake-on-pickup test

--- Sources/accelerometer.c
+++ Sources/accelerometer.c
@@ -10,20 +10,33 @@
 #include <stdio.h>
 #include "i2c.h"
 #include "ports.h"
-#include "systick.h"
+#include "interrupts.h"
 
-#define FXOS8700CQ_I2C_CHANNEL	0
-#define FXOS8700CQ_SLAVE_ADDR	0x1d
+#define FXOS8700CQ_I2C_CHANNEL		0
+#define FXOS8700CQ_SLAVE_ADDR		0x1d
 
-#define FXOS8700CQ_STATUS		0x00
-#define FXOS8700CQ_WHOAMI		0x0D
-#define FXOS8700CQ_XYZ_DATA_CFG	0x0E
-#define FXOS8700CQ_CTRL_REG1	0x2A
-#define FXOS8700CQ_M_CTRL_REG1	0x5B
-#define FXOS8700CQ_M_CTRL_REG2	0x5C
+#define FXOS8700CQ_STATUS			0x00
+#define FXOS8700CQ_INT_SOURCE		0x0C
+#define FXOS8700CQ_WHOAMI			0x0D
+#define FXOS8700CQ_XYZ_DATA_CFG		0x0E
+#define FXOS8700CQ_TRANSIENT_CFG	0x1D
+#define FXOS8700CQ_TRANSIENT_SRC	0x1E
+#define FXOS8700CQ_TRANSIENT_THS	0x1F
+#define FXOS8700CQ_TRANSIENT_COUNT	0x20
+
+#define FXOS8700CQ_CTRL_REG1		0x2A
+#define FXOS8700CQ_CTRL_REG2		0x2B
+#define FXOS8700CQ_CTRL_REG3		0x2C
+#define FXOS8700CQ_CTRL_REG4		0x2D
+#define FXOS8700CQ_CTRL_REG5		0x2E
+
+#define FXOS8700CQ_M_CTRL_REG1		0x5B
+#define FXOS8700CQ_M_CTRL_REG2		0x5C
 
 #define FXOS8700CQ_WHOAMI_VAL	0xC7
 #define FXOS8700CQ_PACKET_SIZE	13
+
+
 
 typedef struct _SensorData {
 	int16_t	x, y, z;
@@ -33,12 +46,30 @@ static int accelInitialised = 0;
 
 static const PortConfig portDPins =
 {
-	PORTE_BASE_PTR,
-	~(PORT_PCR_ISF_MASK | PORT_PCR_MUX_MASK),
-	PORT_PCR_MUX(1),
+	PORTD_BASE_PTR,
+	~(PORT_PCR_ISF_MASK | PORT_PCR_MUX_MASK | PORT_PCR_IRQC_MASK),
+	PORT_PCR_MUX(1) | PORT_PCR_IRQC(0x0a),
 	2,
 	{ 0, 1 }
 };
+
+static volatile int accelerometerInt1Flag = 0;
+
+static void irqHandlerPortCD(uint32_t portCISFR, uint32_t portDISFR)
+{
+	if (portDISFR & 1) {
+		accelerometerInt1Flag++;
+	}
+}
+
+static void accelClearInterrupt()
+{
+	uint8_t intSource = i2cChannelReadByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_INT_SOURCE);
+
+	if (intSource & 0x20) {
+		volatile uint8_t transientStatus = i2cChannelReadByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_TRANSIENT_SRC);
+	}
+}
 
 void accelInit()
 {
@@ -48,11 +79,17 @@ void accelInit()
 		SIM_SCGC5 |= SIM_SCGC5_PORTD_MASK;
 		portInitialise(&portDPins);
 
-		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG1, 0x00);		// place in standby
-		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_M_CTRL_REG1, 0x1f);	// Magnetometer setup 1
-		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_M_CTRL_REG2, 0x20);	// Magnetometer setup 2
-		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_XYZ_DATA_CFG, 0x01);	// Data config
-		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG1, 0x0d);		// Set data rate, low noise, normal reads & enable
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG1, 0x18);			// place in standby, 100Hz data rate
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_TRANSIENT_CFG, 0x1e);		// Latch, X, Y and Z enabled
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_TRANSIENT_THS, 0x03);		// 0.18g threshold
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_TRANSIENT_COUNT, 0x05);	// Debounce to 50ms
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG4, 0x20);			// Enable interrupt
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG5, 0x20);			// Route interrupt
+
+		interruptRegisterPortCDIRQHandler(irqHandlerPortCD);
+		NVIC_EnableIRQ(PORTC_PORTD_IRQn);
+
+		i2cChannelSendByte(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_CTRL_REG1, 0x19);			// activate with 100Hz data rate
 
 		accelInitialised = 1;
 	}
@@ -63,16 +100,12 @@ void accelTest()
 	accelInit();
 
 	if (accelInitialised) {
-		uint8_t dataPacket[FXOS8700CQ_PACKET_SIZE];
-		SensorData sensorData[2];
-
 		while (1) {
-			i2cChannelReadBlock(FXOS8700CQ_I2C_CHANNEL, FXOS8700CQ_SLAVE_ADDR, FXOS8700CQ_STATUS, dataPacket, FXOS8700CQ_PACKET_SIZE);
-			sensorData[0].x = (int16_t)((dataPacket[1] << 8) | dataPacket[2]) >> 2;
-			sensorData[0].y = (int16_t)((dataPacket[3] << 8) | dataPacket[4]) >> 2;
-			sensorData[0].z = (int16_t)((dataPacket[5] << 8) | dataPacket[6]) >> 2;
-
-			printf("%02x: %d %d %d\n", dataPacket[0], sensorData[0].x, sensorData[0].y, sensorData[0].z);
+			__asm("WFI");
+			if (accelerometerInt1Flag) {
+				accelClearInterrupt();
+				break;
+			}
 		}
 	}
 }
